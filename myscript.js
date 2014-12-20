01f18af2e04e243f59cff6026f7316dabd307fbe
Anand Kuchibotla <akuchibotla@gmail.com>
2014-12-20 02:33:31 -0800

    Restructured idea and got a lot more working now

--- myscript.js
+++ myscript.js
@@ -1,37 +1,69 @@
-// Wait for document to load
-document.onreadystatechange = function () {
+var changed = false;
 
-	if (document.readyState == "complete") {
+run = function () {
+	changed = true;
+	if (document.readyState=="complete") {
 
-		// Get body element
-		var body = document.getElementsByTagName("body")[0];
-		body.style.backgroundColor = "black";
+		// Get chat window
+		var win = document.getElementsByClassName("_ksg clearfix")[0];
 
-		// Begin hiding things
-		document.getElementById("content").style.visibility="hidden";
-		document.getElementById("pagelet_bluebar").style.visibility="hidden";
-		document.getElementById("pagelet_sidebar").style.visibility="hidden";
+		// Clear body
+		document.body.innerHTML="";
 
-		var chats = document.getElementsByClassName("fbNubFlyout fbDockChatTabFlyout uiContextualLayerParent");
-		var convos = document.getElementsByClassName("conversation");
+		// Set backgrounds and styles
+		document.body.style.backgroundColor="black";
+		win.style.backgroundColor="black";
+		win.style.fontFamily="Courier";
+		win.style.borderStyle = "none";
+		win.style.color="white";
 
-		for (var i = 0; i < chats.length; i++) {
-
-			var currChat = chats[i];
-			var currConvo = convos[i];
-
-			currChat.getElementsByClassName('titlebarText')[0].style.fontFamily="Courier";
-
-			currChat.getElementsByClassName('conversation')[0].style.fontFamily="Courier";
-			currChat.getElementsByClassName('fbNubFlyoutBody scrollable')[0].style.backgroundColor = "black";
-
-			// Hide availability icon
-			currChat.getElementsByTagName('img')[0].style.visibility="hidden";
-
-			currChat.getElementsByClassName('mls titlebarButtonWrapper rfloat _ohf')[0].style.visibility="hidden";
-
-			currChat.getElementsByClassName('_552n')[0].style.visibility="hidden";
+		// Hide all images
+		images = win.getElementsByTagName("img");
+		for (i = 0; i < images.length; i++) {
+			images[i].style.visibility="hidden";
 		}
+
+		// Unify links
+		links = win.getElementsByTagName("a");
+		for (i = 0; i < links.length; i++) {
+			links[i].style.color="white";
+			links[i].style.fontWeight="normal";
+			links[i].removeAttribute("href");
+			links[i].removeAttribute("data-hovercard");
+		}
+
+		// Unify timestamps
+		timestamps = win.getElementsByTagName("abbr");
+		for (i = 0; i < timestamps.length; i++) {
+			timestamps[i].style.color="white";
+			timestamps[i].style.fontFamily="Courier";
+		}
+
+		// Hide side panel, may change this functionality
+		win.getElementsByClassName("wmMasterView")[0].style.visibility="hidden";
+
+		// Append window to the body
+		document.body.appendChild(win);
+
+		var observer = new MutationObserver(function(mutations) {
+			mutations.forEach(function(mutation) {
+				console.log(mutation);
+			});
+		});
+
+		var config = {attributes: true, childList: true, characterData: true};
+
+		observer.observe(win, config);
 
 	}
 };
+
+// Wait for document to load
+window.onload = run;
+
+// Sometimes document load doesn't work, so this is to ensure the page
+// still performs the action it's supposed to
+setInterval(function () {
+	if (changed === false)
+		run();
+}, 3000);
